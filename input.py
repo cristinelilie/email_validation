9593346117a87177e914202d17c050038e8fdc09
Raul Pasca <pascaraul11@gmail.com>
2025-12-03 11:50:25 +0200

    input modified

--- input.py
+++ input.py
@@ -1,7 +1,13 @@
-first_name = input("Insert first_name")
-last_name = input("Insert last_name")
-email = input("Insert email")
 
-print(first_name)
-print(last_name)
-print(email)
+def input_function():
+    first_name = input("Insert first_name")
+    last_name = input("Insert last_name")
+    email = input("Insert email")
+
+    print(f"first_name: {first_name}, last_name:{last_name}, email:{email}")
+
+    return email
+
+
+
+
