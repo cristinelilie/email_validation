7944cad73fa2ee2ab9d1d3cf9efb13fdb7828e2c
alexandrugoga <alexandru.goga@vodafone.com>
2025-12-03 12:01:10 +0200

    added docstrings

--- src/input.py
+++ src/input.py
@@ -1,5 +1,7 @@
 
 def input_function():
+    """Input function for first name, last name and email"""
+
     first_name = input("Insert first_name")
     last_name = input("Insert last_name")
     email = input("Insert email")
--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 from src.input import input_function
 from src.email_validation import email_validation
 
+
+"""Email validation scripts"""
+
 try:
 
     email = input_function()
